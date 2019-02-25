0ff9855b9c09be687c4ba29e52eae1b964ef6cfc
Kainan Liu <liuknan@bu.edu>
2019-02-25 04:04:23 -0500

    Update test_case1.py

---
+++ test_case1.py
@@ -0,0 +1,16 @@
+import subprocess
+import json
+class TestClass(object):
+    def test_one(self):
+        info_in = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+                                           '-show_format', 'newvideo.mp4'])  # check output
+        info_in = json.loads(info_in)
+        info_out = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+                                            '-show_format', './video/out480p.mp4'])
+        info_out = json.loads(info_out)
+        orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
+        new_duration = float(info_out['streams'][0]['duration'])
+        if orig_duration == new_duration:
+            return True
+        else:
+            return False
