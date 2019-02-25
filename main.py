38dcc568cad480d17f7cec559c78c8b7ba3ec2fd
Kainan Liu <liuknan@bu.edu>
2019-02-24 21:59:13 -0500

    update

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import subprocess
 import os
 import json
+import threading
+ProcessNumber = 5
+
 
 def ffprobe(patin,patout):
     info_in = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams',
-                                       '-show_format', patin])
-    info_in = json.loads(info_in)
+                                       '-show_format', patin])  #check output
+    info_in = json.loads(info_in)   #load the info
     info_out = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams',
                                        '-show_format', patout])
     info_out = json.loads(info_out)
-    orig_duration = float(info_in['streams'][0]['duration'])
+    orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
     new_duration =  float(info_out['streams'][0]['duration'])
     if orig_duration == new_duration:
         return True
@@ -18,14 +21,14 @@ def ffprobe(patin,patout):
 
 
 def video480(pathin):
-    cmd = ['ffmpeg', '-i', pathin,'-r', '30', '-y', '-s', 'hd480', './video/out480p.mp4']
+    cmd = ['ffmpeg', '-i', pathin,'-r', '30', '-y', '-s', 'hd480', './video/out480p.mp4']  # encode
     try:
         subprocess.check_call(cmd)
     except OSError:
         return False
     except subprocess.CalledProcessError as e:
         return False
-    result = ffprobe(path, "./video/out480p.mp4")
+    result = ffprobe(path, "./video/out480p.mp4")   # get the result of duration check
     if result:
         print("Success")
         return True
@@ -51,10 +54,13 @@ def video720(pathin):
         return False
 
 
+
 if __name__ == '__main__':
-    path = "/Users/liuknan/Documents/GitHub/exercise-2-ffmpeg-liuknan/newvideo.mp4"
+    path = "newvideo.mp4"
     if not os.path.exists('./video/'):
         os.mkdir('./video/')
-    Out480p = video480(path)
-    Out720p = video720(path)
+    thread1 = threading.Thread(target=video480, args=(path))  # two threads
+    thread2 = threading.Thread(target=video720, args=(path))
+    thread1.start()
+    thread2.start()
 
