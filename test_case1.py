3e82e9327fc6fa7b3dad914818f0f65e31c43256
Kainan Liu <liuknan@bu.edu>
2019-02-25 04:16:16 -0500

    Update test_case1.py

--- test_case1.py
+++ test_case1.py
@@ -14,3 +14,17 @@ class TestClass(object):
         orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
         new_duration = float(info_out['streams'][0]['duration'])
         assert orig_duration == new_duration
+
+def ffprobe(patin,patout):
+    info_in = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams',
+                                       '-show_format', patin])  #check output
+    info_in = json.loads(info_in)   #load the info
+    info_out = subprocess.check_output(['ffprobe', '-v', 'warning','-print_format', 'json','-show_streams',
+                                       '-show_format', patout])
+    info_out = json.loads(info_out)
+    orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
+    new_duration =  float(info_out['streams'][0]['duration'])
+    if orig_duration == new_duration:
+        return True
+    else:
+        return False
