d63a83b39674ed52f474dd1e12c2e4b01fb68bd5
Kainan Liu <liuknan@bu.edu>
2019-05-16 15:45:59 -0400

    Update test_case1.py

--- test_case1.py
+++ test_case1.py
@@ -1,24 +1,12 @@
 import os
-from queue import Queue
 from main import Video
-import threading
 
 
 def test_one():
         v = Video()
-        input_q = Queue()
-        if not os.path.exists('./video/'):
-            os.mkdir('./video/')
-        input_q.put('./newvideo.mp4')
-        path = input_q.get()
-        v4 = threading.Thread(target=v.video480, args=(path,))
-        v7 = threading.Thread(target=v.video720, args=(path,))
-        v4.start()
-        v7.start()
-        v4.join()
-        result4 = v.ffprobe(path, v.v4output_path)  # check the duration
-        v7.join()
-        result7 = v.ffprobe(path, v.v7output_path)
-        assert result4
-        assert result7
+        v.convert()
+        files = os.listdir("./video/")
+        for file in files:
+            result = v.ffprobe(file[:-8]+".mp4","./video/"+file)  # check the duration
+            assert result
 
