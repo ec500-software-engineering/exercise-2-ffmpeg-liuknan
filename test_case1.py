b97ae32ba19e29db54f18b2457a8207fbb8cb2b5
Kainan Liu <liuknan@bu.edu>
2019-05-16 16:24:30 -0400

    Update test_case1.py

--- test_case1.py
+++ test_case1.py
@@ -1,12 +1,19 @@
+"""
+This test will test the duration of input file and output file.
+"""
 import os
 from main import Video
 
 
 def test_one():
-        v = Video()
-        v.convert()
-        files = os.listdir("./video/")
-        for file in files:
-            result = v.ffprobe(file[:-8]+".mp4","./video/"+file)  # check the duration
-            assert result
+    """
+    To check if the input duration and output duration are equal
+    :return:  no return
+    """
+    v = Video()
+    v.convert()  # convert video
+    files = os.listdir("./video/")  # find output files
+    for file in files:  # check duration
+        result = v.ffprobe(file[:-8]+".mp4","./video/"+file)  # check the duration
+        assert result  # if input and output durations are not equal, assert error
 
